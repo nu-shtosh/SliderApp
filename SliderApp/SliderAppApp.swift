77e8b5f5a580ba274a10dbc61987fddad42a3ba6
ILIA <dubenskiiilia@gmail.com>
2022-12-19 11:41:31 +0100

    Initial Commit

---
+++ SliderApp/SliderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderAppApp.swift
+//  SliderApp
+//
+//  Created by Илья Дубенский on 19.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
