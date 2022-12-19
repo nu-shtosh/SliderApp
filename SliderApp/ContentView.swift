f8c9955aafe05a611d8b8e2d68dcc1ac13a078d0
ILIA <dubenskiiilia@gmail.com>
2022-12-19 21:15:09 +0100

    some marks

--- SliderApp/ContentView.swift
+++ SliderApp/ContentView.swift
@@ -30,7 +30,7 @@ struct ContentView: View {
             .alert(isPresented: $isShowAlert) {
                 Alert(
                     title: Text("Хей!"),
-                    message: Text("Твой результат \(computeScore()) очков!")
+                    message: Text("Твои очки: \(computeScore())!")
                 )
             }
             .padding()
